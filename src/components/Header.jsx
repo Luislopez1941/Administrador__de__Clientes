c523d7767ced3079548ae4878fe2ac985b4c1558
Jorge Luis <jorg3luislopezc1941@hotmail.com>
2023-04-13 00:01:05 -0500

    Deploy 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import header from './Header.css'
+
+const Header = () => {
+  return (
+    <header className='hero'>
+      <div className='hero__container'>
+        <div className='hero__title'>
+            <h2>Seguimiento de Pacientes de <span>Veterinaria</span></h2>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
